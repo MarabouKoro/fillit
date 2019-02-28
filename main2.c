e3c23a2fbc05bbecf1dd96f2ad1215f4924c7148
MarabouKoro <julien.creux.99@gmail.com>
2019-02-28 06:33:08 +0100

    fix de valid_line.c

---
+++ main2.c
@@ -0,0 +1,24 @@
+#include "read.c"
+#include "valid_line.c"
+
+#include <fcntl.h>
+#include <stdio.h>
+
+int		main(void)
+{
+	int		fd = open("unvalid_tetri.txt", O_RDONLY);
+	char	*line;
+	int		ret;
+	int		nb_line = 0;
+
+	while ((ret = ft_read(fd, &line)) > 0)
+	{
+		printf("%d - %s", valid_line(line, nb_line), line);
+		nb_line++;
+		if (nb_line == 5)
+			nb_line = 0;
+		free(line);
+	}
+	close(fd);
+	return (0);
+}
--- valid_line.c
+++ valid_line.c
@@ -6,65 +6,59 @@
 /*   By: jcreux <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/24 19:05:24 by jcreux            #+#    #+#             */
-/*   Updated: 2019/02/24 20:46:53 by jcreux           ###   ########.fr       */
+/*   Updated: 2019/02/28 06:28:46 by jcreux           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-//ft_nb_diese
-
-static int	valid_char(int pos, char c, int nb_line)
+int		check_diese(char *s, int line)
 {
+	int			i;
 	static int	nb_diese = 0;
 
-	if ((pos >= 0 && pos <= 3) && (c == '#' || c == '.'))
+	i = 0;
+	if (line == 4)
+		nb_diese = 0;
+	while (s[i])
 	{
-		if (c == '#')
-			printf("%d - ", nb_diese);
+		if (s[i] == '#')
 			nb_diese++;
-		return (0);
+		i++;
 	}
-	if (pos == 4 && c == '\n')
+	return (nb_diese);
+}
+
+int		valid_char(int pos, char c, int line)
+{
+	if (line >= 0 && line < 4)
 	{
-		return (0);
-	}
-	if (nb_line == 4 && pos == 0 && c == '\n')
-	{
-		if (nb_diese == 4)
-		{
-			printf("%d - ", nb_diese);
-			nb_diese = 0;
+		if (pos >= 0 && pos < 4 && (c == '.' || c == '#'))
 			return (0);
-		}
+		else if (pos == 4 && c == '\n')
+			return (0);
 	}
+	if (line == 4 && pos == 0 && c == '\n')
+		return (0);
 	return (1);
 }
 
-int			valid_line(char *line)
+int		valid_line(char *s, int line)
 {
-	int			i;
-	static int	nb_line = 0;
+	int		i;
+	int		nb_diese;
 
 	i = 0;
-	if (nb_line < 4)
+	nb_diese = check_diese(s, line);
+	while (s[i] != '\n')
 	{
-		while (i < 4)
-		{
-			if (valid_char(i, line[i], nb_line) == 1)
-				return (1);
-			i++;
-		}
-		if (valid_char(i, line[i], nb_line) == 0)
-		{
-			nb_line++;
-			return (0);
-		}
+		if (valid_char(i, s[i], line) == 1)
+			return (1);
+		i++;
 	}
-	if (line[0] == '\n' && nb_line == 4)
-	{
-		nb_line = 0;
-		return (0);
-	}
-	return (1);
+	if (valid_char(i, s[i], line) == 1)
+		return (1);
+	if (nb_diese > 4 || (line == 3 && nb_diese != 4))
+		return (1);
+	return (0);
 }
